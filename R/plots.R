89205f06feb4d13596c18eb4480c662893d09305
Kevin Siwicke <kevin.siwicke@noaa.gov>
2025-06-27 08:04:38 -0800

    try removing extra L border nonsense

--- R/plots.R
+++ R/plots.R
@@ -40,7 +40,6 @@ make_plots <- function(sta, depth, chx, lengths, lenE, lenO, StnLoc, stnd) {
       ggplot2::theme(axis.title = ggplot2::element_text(size = 14),
                      axis.text = ggplot2::element_text(size = 12),
                      strip.background = ggplot2::element_blank(),
-                     panel.border = llsbios::theme_L_border(),
                      panel.grid.major = ggplot2::element_blank(),
                      panel.grid.minor = ggplot2::element_blank(),
                      strip.text = ggplot2::element_text(size = 12))
@@ -60,7 +59,6 @@ make_plots <- function(sta, depth, chx, lengths, lenE, lenO, StnLoc, stnd) {
       ggplot2::theme(axis.title = ggplot2::element_text(size = 14),
                      axis.text = ggplot2::element_text(size = 12),
                      strip.background = ggplot2::element_blank(),
-                     panel.border = llsbios::theme_L_border(),
                      panel.grid.major = ggplot2::element_blank(),
                      panel.grid.minor = ggplot2::element_blank(),
                      strip.text = ggplot2::element_text(size = 12))
@@ -78,161 +76,4 @@ make_plots <- function(sta, depth, chx, lengths, lenE, lenO, StnLoc, stnd) {
     ggplot2::theme_bw()
 
   ggplot2::ggsave(location, file = paste0("Today/Sta", sta, "_HaulPos.png"))
-}
-
-
-
-
-#' Code duplicated from ggplot2 source (not exposed to wider namespace) for convenience
-#'
-#' @param x
-#'
-#' @return
-#' @export
-#'
-#' @examples
-len0_null <- function(x) {
-  if (length(x) == 0)  NULL
-  else                 x
-}
-
-#' Title
-#'
-#' @param type
-#' @param colour
-#' @param size
-#' @param linetype
-#'
-#' @return
-#' @export
-#'
-#' @examples
-theme_border <- function(
-    type = c("left", "right", "bottom", "top", "none"),
-    colour = "black", size = 1, linetype = 1) {
-  # use with e.g.: ggplot(...) + opts( panel.border=theme_border(type=c("bottom","left")) ) + ...
-  type <- match.arg(type, several.ok=TRUE)
-  structure(
-    list(type = type, colour = colour, size = size, linetype = linetype),
-    class = c("theme_border", "element_blank", "element")
-  )
-}
-
-
-
-#' Title
-#'
-#' @param element
-#' @param x
-#' @param y
-#' @param width
-#' @param height
-#' @param type
-#' @param colour
-#' @param size
-#' @param linetype
-#' @param ...
-#'
-#' @return
-#' @export
-#'
-#' @examples
-element_grob.theme_border <- function(
-    element, x = 0, y = 0, width = 1, height = 1,
-    type = NULL,
-    colour = NULL, size = NULL, linetype = NULL,
-    ...) {
-  if (is.null(type)) type = element$type
-  xlist <- c()
-  ylist <- c()
-  idlist <- c()
-  if ("bottom" %in% type) { # bottom
-    xlist <- append(xlist, c(x, x+width))
-    ylist <- append(ylist, c(y, y))
-    idlist <- append(idlist, c(1,1))
-  }
-  if ("top" %in% type) { # top
-    xlist <- append(xlist, c(x, x+width))
-    ylist <- append(ylist, c(y+height, y+height))
-    idlist <- append(idlist, c(2,2))
-  }
-  if ("left" %in% type) { # left
-    xlist <- append(xlist, c(x, x))
-    ylist <- append(ylist, c(y, y+height))
-    idlist <- append(idlist, c(3,3))
-  }
-  if ("right" %in% type) { # right
-    xlist <- append(xlist, c(x+width, x+width))
-    ylist <- append(ylist, c(y, y+height))
-    idlist <- append(idlist, c(4,4))
-  }
-  if (length(type)==0 || "none" %in% type) { # blank; cannot pass absence of coordinates, so pass a single point and use an invisible line
-    xlist <- c(x,x)
-    ylist <- c(y,y)
-    idlist <- c(5,5)
-    linetype <- "blank"
-  }
-  gp <- grid::gpar(lwd = len0_null(size * 2.834646),
-                   col = colour,
-                   lty = linetype)
-  element_gp <- grid::gpar(lwd = len0_null(element$size * 2.834646),
-                           col = element$colour,
-                           lty = element$linetype)
-  grid::polylineGrob(
-    x = xlist, y = ylist, id = idlist, ..., default.units = "npc",
-    gp = utils::modifyList(element_gp, gp),
-  )
-}
-
-
-
-
-#' For convenience: "L" (left + bottom) border
-#'
-#' @param colour
-#' @param size
-#' @param linetype
-#'
-#' @return
-#' @export
-#'
-#' @examples
-theme_L_border <- function(colour = "black", size = 1, linetype = 1) {
-  # use with e.g.: ggplot(...) + theme( panel.border=theme_L_border() ) + ...
-  structure(
-    list(colour = colour, size = size, linetype = linetype),
-    class = c("theme_L_border", "element_blank", "element")
-  )
-}
-
-#' Title
-#'
-#' @param element
-#' @param x
-#' @param y
-#' @param width
-#' @param height
-#' @param colour
-#' @param size
-#' @param linetype
-#' @param ...
-#'
-#' @return
-#' @export
-#'
-#' @examples
-element_grob.theme_L_border <- function(
-  element, x = 0, y = 0, width = 1, height = 1,
-    colour = NULL, size = NULL, linetype = NULL,
-    ...) {
-  gp <- grid::gpar(lwd = len0_null(size * 2.834646),
-                   col = colour,
-                   lty = linetype)
-  element_gp <- grid::gpar(lwd = len0_null(element$size * 2.834646),
-                           col = element$colour,
-                           lty = element$linetype)
-  grid::polylineGrob(
-    x = c(x+width, x, x), y = c(y,y,y+height), ..., default.units = "npc",
-    gp = utils::modifyList(element_gp, gp),
-  )
 }
