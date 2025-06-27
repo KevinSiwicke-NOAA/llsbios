bb7211db11a13b6d3996835cebc26caa6189dab7
Kevin Siwicke <kevin.siwicke@noaa.gov>
2025-06-27 07:09:18 -0800

    corrected package name

--- R/qaqc.R
+++ R/qaqc.R
@@ -8,9 +8,9 @@
 #'
 #' @examples
 qaqc <- function(channel, station) {
-  dat <- qaqc:::get_data(channel = channel, sta_num = station)
+  dat <- llsbios:::get_data(channel = channel, sta_num = station)
 
-  plot_dat <- qaqc:::make_plot_data(depth = dat[[1]], position = dat[[2]], length = dat[[3]])
+  qaqc_plot_dat <- llsbios:::make_plot_data(depth = dat[[1]], position = dat[[2]], length = dat[[3]])
 
-  make_plots(depth = plot_dat[[1]], chx = plot_dat[[2]], len3 = plot_dat[[3]], testE = plot_dat[[4]], testE = plot_dat[[5]], StnLoc = plot_dat[[6]])
+  make_plots(depth = qaqc_plot_dat[[1]], chx = qaqc_plot_dat[[2]], len3 = qaqc_plot_dat[[3]], testE = qaqc_plot_dat[[4]], testE = qaqc_plot_dat[[5]], StnLoc = qaqc_plot_dat[[6]])
 }
